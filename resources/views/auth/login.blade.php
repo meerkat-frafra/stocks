9a81c75c40e0131e5a74e45aee9486c6dace2bcd
nonoichi123 <nonoichi123@gmail.com>
2018-02-10 20:51:42 +0900

    Social buttom comment in.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,11 +42,11 @@
                     {!! Form::close() !!}
 
                 </div>
-                <div class="mdl-card__actions mdl-card--border">
+                <!--<div class="mdl-card__actions mdl-card--border">
 
                     @include('partials.socials-icons')
 
-                </div>
+                </div>—->
                 <div class="mdl-card__actions mdl-card--border">
 
                     {!! HTML::link(route('password.request'), trans('auth.forgot'), array('id' => 'forgot', 'class' => 'mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect left')) !!}
