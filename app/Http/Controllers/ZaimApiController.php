6a9a9b3217e2806c4e2e5b722ca14cfe1964eac6
nonoichi123 <nonoichi123@gmail.com>
2018-03-04 16:32:51 +0900

    Library をAutoloadするように設定

--- app/Http/Controllers/ZaimApiController.php
+++ app/Http/Controllers/ZaimApiController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers;
 
-ini_set('display_errors', "On");
-ini_set('include_path', '.:/app/.heroku/php/lib/php:/app/library');
+//ini_set('display_errors', "On");
+//ini_set('include_path', '.:/app/.heroku/php/lib/php:/app/library');
 
-require_once('HTTP/OAuth/Consumer.php');
+//require_once('HTTP/OAuth/Consumer.php');
 
 use DB;
 use Request;
 use Log;
 use Stock;
+use Lib
 
 class ZaimApiController extends Controller
 {
@@ -49,12 +50,12 @@ class ZaimApiController extends Controller
 		$content = '';
 		try {
   			// Initialize HTTP_OAuth_Consumer
-			$oauth = new HTTP_OAuth_Consumer($consumer_key, $consumer_secret);
+			$oauth = new Lib\HTTP_OAuth_Consumer($consumer_key, $consumer_secret);
  
 			// Enable SSL
-			$http_request = new HTTP_Request2();
+			$http_request = new Lib\HTTP_Request2();
 			$http_request->setConfig('ssl_verify_peer', false);
-			$consumer_request = new HTTP_OAuth_Consumer_Request;
+			$consumer_request = new Lib\HTTP_OAuth_Consumer_Request;
 			$consumer_request->accept($http_request);
 			$oauth->accept($consumer_request);
   
